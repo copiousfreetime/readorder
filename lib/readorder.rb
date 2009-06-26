d3bdf34497fd18de6d6b2d423296e82c0cc01ffe
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-06-26 14:22:34 -0600

    initial import

---
+++ lib/readorder.rb
@@ -0,0 +1,11 @@
+#--
+# Copyright (c) 2009 Jeremy Hinegardner
+# All rights reserved.  See LICENSE and/or COPYING for details.
+#++
+
+module Readorder
+  class Error < StandardError; end
+end
+
+require 'readorder/version'
+require 'readorder/paths'
